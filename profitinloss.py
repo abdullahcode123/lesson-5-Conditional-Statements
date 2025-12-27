f130b130f7781a45b5522ff81ccb88524634a7df
abdullahcoding123 <abdullahshahzadturk@gmail.com>
2025-12-27 19:56:36 +0500

    conditional stateMents

---
+++ profitinloss.py
@@ -0,0 +1,7 @@
+cp=float(input("Please enter the cost prise"))
+sp=float(input("Please enter the selling prise"))
+if(sp>cp):
+    a=sp-cp
+    print("total profit = {0}".format(a))
+else:
+    print("NO PROFITTT !!!!!")
